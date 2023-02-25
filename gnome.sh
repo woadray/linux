3033f34760c8e5891444834adb43c83aeffc18ad
woadray <35317811+woadray@users.noreply.github.com>
2023-02-25 22:30:53 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -32,27 +32,22 @@ dnf --setopt=install_weak_deps=False --best install -y kitty \
 						       yt-dlp-fish-completion \
 						       fzf \
 						       openssl \
+						       fd-find \
+						       bat \
 						       gnome-tweaks \
 						       gnome-extensions-app \
 						       gnome-browser-connector \
 						       papirus-icon-theme \
 						       breeze-cursor-theme
 
-# Tools
-dnf --setopt=install_weak_deps=False --best install -y gcc \
-						       gdb \
-						       valgrind \
-						       nodejs \
-						       npm
-
-dnf --setopt=install_weak_deps=False --best install -y fd-find \
-						       bat \
-						       ripgrep \
-						       tree-sitter-cli
-						       
 # Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 dnf install -y google-chrome-stable_current_x86_64.rpm
+
+# VS Code
+# rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+# dnf install -y code
 
 # adw-gtk3
 wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.3/adw-gtk3v4-3.tar.xz
