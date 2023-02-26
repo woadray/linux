c05626e4a0ce0888e1e0cefafad63f1cd8783b6b
woadray <35317811+woadray@users.noreply.github.com>
2023-02-26 01:24:41 +0100

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -44,11 +44,6 @@ dnf --setopt=install_weak_deps=False --best install -y kitty \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 dnf install -y google-chrome-stable_current_x86_64.rpm
 
-# VS Code
-# rpm --import https://packages.microsoft.com/keys/microsoft.asc
-# sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-# dnf install -y code
-
 # adw-gtk3
 wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.3/adw-gtk3v4-3.tar.xz
 
