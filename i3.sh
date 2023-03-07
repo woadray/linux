ab7ea8c4d526f5f40820699ccf30afe19f265584
woadray <35317811+woadray@users.noreply.github.com>
2023-03-07 18:57:42 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir /mnt/usb/
+
 # RPM FUSION
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
                https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
