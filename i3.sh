85a70605fa1fd0fc32a46115262f1a0217402455
woadray <35317811+woadray@users.noreply.github.com>
2023-03-08 14:08:45 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -55,20 +55,23 @@ dnf --setopt=install_weak_deps=False --best install -y kitty \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 dnf install -y google-chrome-stable_current_x86_64.rpm
 
+# VS Code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+dnf --setopt=install_weak_deps=False --best install -y code
+
+# SETUP
+dnf --setopt=install_weak_deps=False --best install -y python3-autopep8 \
+                                                       java-17-openjdk-devel maven \
+                                                       gcc gdb valgrind \
+                                                       nodejs npm
+
 # RICE
 dnf --setopt=install_weak_deps=False --best install -y lxappearance \
                                                        arc-theme \
                                                        papirus-icon-theme \
                                                        breeze-cursor-theme
-
-# SETUP
-dnf --setopt=install_weak_deps=False --best install -y python3-pip \
-                                                       gcc \
-                                                       gcc-c++ \
-                                                       gdb \
-                                                       valgrind \
-                                                       nodejs \
-                                                       npm 
 
 # MISC
 # xorg.conf
